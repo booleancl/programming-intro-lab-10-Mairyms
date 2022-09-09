7ad4851d4e9a2b47b8c5ade40d315732022fb1a0
Mairym Salcedo <mairymsalcedo@gmail.com>
2022-09-08 22:06:27 -0400

    se agrego el codigo de notebook

---
+++ src/01-simple-cli/notebook.py
@@ -0,0 +1,32 @@
+from note import Note
+
+class Notebook:
+    def __init__(self):
+        self.notes = []
+    
+    def add_note (self, content, *tags):
+        note = Note (content, *tags)
+        self.notes.append(note)
+    
+    def edit_content(self, id, new_content):
+        for note in self.notes:
+            if note.id == id:
+                note.content = new_content
+
+    def edit_tags(self, id, *new_tags):
+        for note in self.notes:
+            if note.id == id:
+                note.tags = [*new_tags]
+
+notebook = Notebook()
+notebook.add_note("Hello World", "salute", "hi")
+notebook.add_note("Hello World", "salute", "hi")
+notebook.add_note("Hello World", "salute", "hi")
+#print(len(notebook.notes))
+print(notebook.notes[0].content)
+notebook.edit_content(1, 'chao')
+print(notebook.notes[0].content)
+
+notebook.edit_tags(1, 'viernes')
+print(notebook.notes[0].tags)
+
